26df2dcfc8f24e9645fd644d900ed61eda23c1ca
zhouya0 <yao.zhoupolytech@gmail.com>
2019-12-30 10:30:24 +0800

    Add chapter 5

---
+++ chapter5/eof.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	for {
+		r, _, err := in.ReadRune()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Printf("read failed:%v", err)
+		}
+		fmt.Println(r)
+	}
+	fmt.Print("Read stops")
+}
