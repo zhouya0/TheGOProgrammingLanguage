8a2ae33e7798cbf5c024c8032f430e7970b99b25
zhouya0 <yao.zhoupolytech@gmail.com>
2020-01-10 10:38:32 +0800

    add chapter 8

---
+++ chapter8/fib.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func spinner(delay time.Duration, wg *sync.WaitGroup) {
+	for {
+		for _, r := range `-\|/` {
+			fmt.Printf("\r%c", r)
+			time.Sleep(delay)
+		}
+	}
+	defer wg.Done()
+}
+
+func fib(x int) int {
+	if x < 2 {
+		return x
+	}
+	return fib(x-1) + fib(x-2)
+}
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go spinner(100*time.Millisecond, &wg)
+	wg.Wait()
+
+	// const n = 45
+	// fibN := fib(n)
+	// fmt.Printf("\rFibonacci(%d) = %d\n", n, fibN)
+}
