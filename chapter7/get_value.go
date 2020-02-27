6964f80f7638245947647ed208b25e58a482548a
zhouya0 <yao.zhoupolytech@gmail.com>
2020-02-27 10:24:08 +0800

    Add interface get value

---
+++ chapter7/get_value.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+type iface struct {
+	itab, data uintptr
+}
+
+func main() {
+	var a interface{} = nil
+
+	var b interface{} = (*int)(nil)
+
+	x := 5
+	var c interface{} = (*int)(&x)
+
+	// 类型转化！！！
+	ia := *(*iface)(unsafe.Pointer(&a))
+	ib := *(*iface)(unsafe.Pointer(&b))
+	ic := *(*iface)(unsafe.Pointer(&c))
+
+	fmt.Println(ia, ib, ic)
+
+	fmt.Println(*(*int)(unsafe.Pointer(ic.data)))
+}
